eb29b39020c86bccca9e27be45f18c28b79e8101
Stephen Williams <stephen.williams@10xgenomics.com>
2017-03-02 09:38:44 -0800

    Update sge_mummer.sh

--- sge_mummer.sh
+++ sge_mummer.sh
@@ -1,28 +1,30 @@
-!/bin/bash
+#!/bin/bash
 # written by Fritz Sedlazeck, for questions: fsedlazeck@cshl.edu
 
 min_args=3
+#path to the sge_mumer install directory:
+script_path=/mnt/home/stephen/Apps/sge_mummer/
 #path to the mumer install directory:
-script_path=/seq/schatz/fritz/sge_mummer2/sge_mummer/
-#path to the mumer install directory:
-mummer_path=/sonas-hs/schatz/hpc/home/fsedlaze/programs/MUMmer3.23/
+mummer_path=/mnt/home/stephen/.linuxbrew/Cellar/mummer/3.23/
 
 #check if user specified the requried parameters
-if [ $# -eq $min_args ]; then        
+if [ $# -eq $min_args ]; then
 	#get full paths:
 	ref=$(readlink -f $1)
 	query=$(readlink -f $2)
 	work=$(readlink -f $3)
+echo referencd:$ref
+echo query:$query
+echo workingdir:$work
 
 	#prepare ref file:
 	mkdir -p $work
 	echo "split up ref"
 	$script_path/explode_fasta.pl $ref $work
-	
 	#create sge job:
 	num=$(wc -l < $work/summary)
 	sed "s/NUMMER/$num/g" $script_path/sge_mummer_helper.sh > $work'/mummer.sh'
-	qsub -sync y -N mummersplit -o $work/output.txt -e $work/error.txt -v PATHMUM=\'$mummer_path\',REF_FASTA_LIST=\'$work/summary\',QRY=\'$query\',WORK=\'$work\' $work'/mummer.sh'
+	qsub -sync y -N mummersplit -o $work/output.txt -e $work/error.txt -v PATHMUM=$mummer_path,REF_FASTA_LIST=$work/summary,QRY=$query,WORK=$work $work/mummer.sh
 	echo "wating for mummer to finish"
 	echo "reconstructing file"
 	#create a merged file:
@@ -44,7 +46,7 @@ if [ $# -eq $min_args ]; then
 	for i in $(cat $work/summary)
 	do
 		rm $i
-		rm -r $work/$tmp 
+		rm -r $work/$tmp
 		tmp=$((tmp+1))
 	done
 	rm $work/summary
@@ -56,4 +58,3 @@ else
 	echo "File path of the query file"
 	echo "Working directory"
 fi
-
